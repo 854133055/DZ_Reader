495ce31b299940b2a088350530595dceb85e1efa
lml <854133055@qq.com>
2016-08-01 18:14:34 +0800

    增加二维码功能

---
+++ app/src/main/java/com/example/lml/dz_reader/ContentActivity.java
@@ -0,0 +1,41 @@
+package com.example.lml.dz_reader;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class ContentActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content);
+
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("url");
+        mWebView = (WebView)this.findViewById(R.id.webView);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                //get the newProgress and refresh progress bar
+            }
+
+        });
+
+        mWebView.loadUrl(data);
+    }
+
+
+}
--- app/src/main/java/com/example/lml/dz_reader/MainActivity.java
+++ app/src/main/java/com/example/lml/dz_reader/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.lml.dz_reader;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -33,8 +33,9 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+                startActivity(intent);
+
             }
         });
 
@@ -94,7 +95,7 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
